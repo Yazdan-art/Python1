34014c2bbcbb495a0986da0c833121171f1e64c0
Yazdan <yazdan.ghezelbash@gmail.com>
2023-10-12 20:02:10 +0330

    task1

--- print(2+2).py
+++ print(2+2).py
@@ -1,5 +1,43 @@
-print(2+2)
-print ("hello_python")
-print("hi_git")
-print(5.3+36)
-# __________________________________________
+# print(2+2)
+# print ("hello_python")
+# print("hi_git")
+# print(5.3+36)Hell
+#_______________________________
+# print("hi world")
+# hour =1
+# rate=1000
+# payment=hour*rate
+# print("\n")
+# print(payment)
+#_______________________________
+# p='Nima'+'\n'+'Talebi'
+# print(p)
+# print("yazdan" + "\n"+ "ghezelbash")
+# dd=1+4
+# print(dd)
+#_______________________________
+# nam=input("who are you?")
+# print('welcome ',nam)
+#_______________________________
+# h=input("pleas tell me about tim")
+# print(type(h))
+# rate=input("pleas enter you rate")
+# pay=int(h)*int(rate)
+# print(pay)
+#________________________________________
+#for nothing
+# print(int(input("pleas send your time")))
+#_____________________________________________-
+# h=int(input("enter"))
+# r=int(input ("enter"))
+# print(h*r)
+#__________________conditions___________________
+h=int(input("enter h"))
+rate=int(input("enter rate"))
+if h<=160 :
+    print(h*rate)
+else :
+    r=h-160
+    
+        
+    
