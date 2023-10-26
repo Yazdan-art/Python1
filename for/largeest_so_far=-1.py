b0e4690f2a5bceea9d01c967701c71b25e4f100a
Yazdan <yazdan.ghezelbash@gmail.com>
2023-10-26 09:43:35 +0330

    for

---
+++ for/largeest_so_far=-1.py
@@ -0,0 +1,16 @@
+# largeest_so_far=-1
+# print ('before',largeest_so_far)
+# for the_num in [9,41,12,3,74,15]:
+#     if the_num>largeest_so_far:
+#         largeest_so_far=the_num
+#     print(largeest_so_far,the_num)    
+# print('after',largeest_so_far)    
+#___________________________________
+sum=0
+count=0
+
+print('before',sum)
+for the_num in [9,41,12,3,74,15]:
+   sum+=the_num
+   count+=1
+print('avrage = ' ,sum/count)   
