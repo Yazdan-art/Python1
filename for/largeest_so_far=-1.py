d9f7aa6791c72cf90d8ceff9bb9fc382ee5d4739
Yazdan <yazdan.ghezelbash@gmail.com>
2023-11-02 15:43:09 +0330

    String

--- for/largeest_so_far=-1.py
+++ for/largeest_so_far=-1.py
@@ -15,8 +15,19 @@
 #    count+=1
 # print('avrage = ' ,sum/count)  
 #___________________________________
-for the_num in [9,41,12,3,74,15,19,20,28,64,70,90]:
-    if the_num>18:
-        print(the_num)
-    if the_num==90:
-          print('there is a person with 90 age')  
+# for the_num in [9,41,12,3,74,15,19,20,28,64,70,90]:
+#     if the_num>18:
+#         print(the_num)
+#     if the_num==90:
+#         print('there is a person with 90 age')  
+#___________________________________  
+smallest=None
+print("before")
+for the_num in [9,41,12,3,74,15,19,20,28,64,70,90]: 
+    if smallest is None:
+        smallest=the_num
+    elif the_num<smallest:
+        the_num=smallest 
+        print("smallest",the_num)
+print("after ",the_num)        
+              
---
+++ str1='hellow'.py
@@ -0,0 +1,81 @@
+# str1='hellow'
+# str2="world"
+# bob =str1+str2
+# print(bob)
+# str3="123"
+# x=int(str3)+1
+# print(x)
+
+#___________________
+# age=int(input("enter your age"))
+# if(age>18):
+#     print("age >18")
+# elif(age==18):   
+#     print("age==18") 
+# else:
+#     print("age<18")
+#______________________
+# fruit="banana"
+# letter=fruit[1]
+# print(letter)
+# print("\n")
+# x=3
+# print(fruit[x-8])
+# print(len(fruit))
+#_________________________
+# fruit="banana"
+# index=0
+# while index<len(fruit):
+#     letter=fruit[index]
+#     print(index,letter)
+#     index=index+1
+#_________________________
+# fruit="banana"
+# for latter in fruit:
+#     print(latter)
+    
+#_________________________    
+# fruit="banana"
+# count =0
+# for letter in fruit:
+#     if (letter=='a'):
+#         count=count+1
+# print(count) 
+
+#__________________________________
+# s="Monty python"
+# print(s[:])
+# print(s[0:1])
+# print(s[0:5])
+# print(s[:2])
+# print(s[5:])
+#________________
+# word=input("enter your desire word")
+# userName="yazdan"
+# if word==userName:
+#     print("wellcome")
+# elif word>userName:
+#     print("your word,"+word+"comes before userName")
+# elif word<userName:
+#     print("your word,"+word+"comes after userName")    
+# else :
+#     print("wellcome"+userName)
+#___________________________________
+# greet="yazdan ghezelbash"
+# lower_greet=greet.lower()
+# print(lower_greet)
+# print(greet.upper())
+# print(type(greet))
+# print(dir(greet))
+# split1=greet.split()
+# print(split1)
+# print(greet.split())
+#___________________________________
+# fruit="banana"
+# fin=fruit.find("an")
+# print(fin)
+#___________________________________
+fruit="banana"
+rep=fruit.replace('a','A',2)
+print(rep)
+
